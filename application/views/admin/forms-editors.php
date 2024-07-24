56b6c1f4e246555ba03e46a9a73fa27e4649c2d8
wildanu <wildansbh@gmail.com>
2024-07-24 15:40:21 +0700

    form post

--- application/views/admin/forms-editors.php
+++ application/views/admin/forms-editors.php
@@ -17,77 +17,118 @@
   <main id="main" class="main">
 
     <div class="pagetitle">
-      <h1>Form Editors</h1>
-      <nav>
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-          <li class="breadcrumb-item">Forms</li>
-          <li class="breadcrumb-item active">Editors</li>
-        </ol>
-      </nav>
+      <h1>Form Post</h1>
     </div><!-- End Page Title -->
 
     <section class="section">
       <div class="row">
-        <div class="col-lg-6">
+        <div class="col-lg-12">
 
           <div class="card">
             <div class="card-body">
-              <h5 class="card-title">Quill Editor Default</h5>
-
-              <!-- Quill Editor Default -->
-              <div class="quill-editor-default">
-                <p>Hello World!</p>
-                <p>This is Quill <strong>default</strong> editor</p>
-              </div>
-              <!-- End Quill Editor Default -->
-
-            </div>
-          </div>
-
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Quill Editor Bubble</h5>
-
-              <!-- Quill Editor Bubble -->
-              <p>Select some text to display options in poppovers</p>
-              <div class="quill-editor-bubble">
-                <p>Hello World!</p>
-                <p>This is Quill <strong>bubble</strong> editor</p>
-              </div>
-              <!-- End Quill Editor Bubble -->
-
-            </div>
-          </div>
-
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Quill Editor Full</h5>
-
-              <!-- Quill Editor Full -->
-              <p>Quill editor with full toolset</p>
-              <div class="quill-editor-full">
-                <p>Hello World!</p>
-                <p>This is Quill <strong>full</strong> editor</p>
-              </div>
-              <!-- End Quill Editor Full -->
-
-            </div>
-          </div>
-
-        </div>
-
-        <div class="col-lg-6">
-
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">TinyMCE Editor</h5>
-
-              <!-- TinyMCE Editor -->
-              <textarea class="tinymce-editor">
-                <p>Hello World!</p>
-                <p>This is TinyMCE <strong>full</strong> editor</p>
-              </textarea><!-- End TinyMCE Editor -->
+              <h5 class="card-title">Post</h5>
+              <button type="button" class="btn btn-warning">
+                <i class="bi bi-plus-lg"></i>
+                Tambah Post</button>
+              <!-- Table with stripped rows -->
+              <table class="table datatable">
+                <thead>
+                  <tr>
+                    <th>No</th>
+                    <th>judul</th>
+                    <th>Post By</th>
+                    <th data-type="date" data-format="YYYY/DD/MM">Tahun</th>
+                    <th>Aksi</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Unity Pugh</td>
+                    <td>9958</td>
+                    <td>Curicó</td>
+                    <td>2005/02/11</td>
+                    <td><button type="button" class="btn btn-danger"><button type="button" class="btn btn-success"></td>
+                  </tr>
+                  <tr>
+                    <td>Theodore Duran</td>
+                    <td>8971</td>
+                    <td>Dhanbad</td>
+                    <td>1999/04/07</td>
+                    <td>97%</td>
+                  </tr>
+                  <tr>
+                    <td>Kylie Bishop</td>
+                    <td>3147</td>
+                    <td>Norman</td>
+                    <td>2005/09/08</td>
+                    <td>63%</td>
+                  </tr>
+                  <tr>
+                    <td>Willow Gilliam</td>
+                    <td>3497</td>
+                    <td>Amqui</td>
+                    <td>2009/29/11</td>
+                    <td>30%</td>
+                  </tr>
+                  <tr>
+                    <td>Blossom Dickerson</td>
+                    <td>5018</td>
+                    <td>Kempten</td>
+                    <td>2006/11/09</td>
+                    <td>17%</td>
+                  </tr>
+                  <tr>
+                    <td>Elliott Snyder</td>
+                    <td>3925</td>
+                    <td>Enines</td>
+                    <td>2006/03/08</td>
+                    <td>57%</td>
+                  </tr>
+                  <tr>
+                    <td>Castor Pugh</td>
+                    <td>9488</td>
+                    <td>Neath</td>
+                    <td>2014/23/12</td>
+                    <td>93%</td>
+                  </tr>
+                  <tr>
+                    <td>Pearl Carlson</td>
+                    <td>6231</td>
+                    <td>Cobourg</td>
+                    <td>2014/31/08</td>
+                    <td>100%</td>
+                  </tr>
+                  <tr>
+                    <td>Deirdre Bridges</td>
+                    <td>1579</td>
+                    <td>Eberswalde-Finow</td>
+                    <td>2014/26/08</td>
+                    <td>44%</td>
+                  </tr>
+                  <tr>
+                    <td>Daniel Baldwin</td>
+                    <td>6095</td>
+                    <td>Moircy</td>
+                    <td>2000/11/01</td>
+                    <td>33%</td>
+                  </tr>
+                  <tr>
+                    <td>Phelan Kane</td>
+                    <td>9519</td>
+                    <td>Germersheim</td>
+                    <td>1999/16/04</td>
+                    <td>77%</td>
+                  </tr>
+                  <tr>
+                    <td>Quentin Salas</td>
+                    <td>1339</td>
+                    <td>Los Andes</td>
+                    <td>2011/26/01</td>
+                    <td>49%</td>
+                  </tr>
+                </tbody>
+              </table>
+              <!-- End Table with stripped rows -->
 
             </div>
           </div>
@@ -97,6 +138,7 @@
     </section>
 
   </main><!-- End #main -->
+  <!-- End #main -->
 
 
   <!-- ======= Footer ======= -->
