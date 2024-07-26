00b30879c08d0feab26259929c7296839e8c24c4
josikie <kiejosi12@gmail.com>
2024-07-26 20:20:19 +0700

    Updated Profiles

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,13 +1,12 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class User extends MY_Controller {
+class Admin extends MY_Controller {
 
     public function index(){
         $data['user'] = $this->db->get_where('users', ['email' 
         => $this->session->userdata('email')])->row_array();
-        $data['role'] = "User";
-
-        $this->load->view('/user/dashboard', $data);
+        $data['role'] = "Admin";
+        $this->load->view('/admin/dashboard', $data);
     }
 }
--- application/views/admin/includes/sidebar-admin.php
+++ application/views/admin/includes/sidebar-admin.php
@@ -1,78 +1,42 @@
 <aside id="sidebar" class="sidebar">
 
   <ul class="sidebar-nav" id="sidebar-nav">
+    <li class="nav-item">
+      <a class="nav-link"  href="#">
+        <i class="bi bi-person-fill"></i><span>Profile</span>
+      </a>
+    </li>
 
     <li class="nav-item">
-      <a class="nav-link " href="dashboard">
-        <i class="bi bi-grid"></i>
-        <span>Dashboard</span>
+      <a class="nav-link" href="#">
+        <i class="bi bi-pencil-square"></i><span>Post</span>
       </a>
-    </li><!-- End Dashboard Nav -->
+    </li>
 
     <li class="nav-item">
       <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
-        <i class="bi bi-pencil-square"></i><span>Manajement Post</span><i class="bi bi-chevron-down ms-auto"></i>
+        <i class="bi bi-pencil-square"></i><span>Approval</span><i class="bi bi-chevron-down ms-auto"></i>
       </a>
       <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
         <li>
-          <a href="addpost">
-            <i class="bi bi-plus-square"></i><span>Add Post</span>
-          </a>
+            <a href="#">
+              <i class="bi bi-clock"></i><span>Waiting for Review</span>
+            </a>
         </li>
         <li>
-          <a href="components-accordion.html">
-            <i class="bi bi-pencil"></i><span>Edit Post</span>
+            <a href="#">
+              <i class="bi bi-check-lg"></i><span>Approved</span>
+            </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="bi bi-x-lg"></i><span>Waiting for Review</span>
           </a>
         </li>
       </ul>
     </li><!-- End Components Nav -->
-
-    <li class="nav-item">
-      <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
-        <i class="bi bi-person"></i><span>Manajement User</span><i class="bi bi-chevron-down ms-auto"></i>
-      </a>
-      <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-        <li>
-          <a href="forms-elements.html">
-            <i class="bi bi-circle"></i><span>Form Elements</span>
-          </a>
-        </li>
-        <li>
-          <a href="forms-layouts.html">
-            <i class="bi bi-circle"></i><span>Form Layouts</span>
-          </a>
-        </li>
-        <li>
-          <a href="forms-editors.html">
-            <i class="bi bi-circle"></i><span>Form Editors</span>
-          </a>
-        </li>
-        <li>
-          <a href="forms-validation.html">
-            <i class="bi bi-circle"></i><span>Form Validation</span>
-          </a>
-        </li>
-      </ul>
-    </li><!-- End Forms Nav -->
-
-    <li class="nav-item">
-      <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
-        <i class="bi bi-gear"></i><span>Manajement Sistem</span><i class="bi bi-chevron-down ms-auto"></i>
-      </a>
-      <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-        <li>
-          <a href="tables-general.html">
-            <i class="bi bi-circle"></i><span>General Tables</span>
-          </a>
-        </li>
-        <li>
-          <a href="tables-data.html">
-            <i class="bi bi-circle"></i><span>Data Tables</span>
-          </a>
-        </li>
-      </ul>
-    </li><!-- End Tables Nav -->
-
+    
+    <!-- End Components Nav -->
   </ul>
 
 </aside>
