721bd7b56be0200fd33f13b737d4ad599061d334
josikie <kiejosi12@gmail.com>
2024-07-26 21:39:33 +0700

    Separated Menu for Admin and User role

--- application/views/admin/includes/sidebar-admin.php
+++ application/views/admin/includes/sidebar-admin.php
@@ -1,79 +1,66 @@
 <aside id="sidebar" class="sidebar">
-
-  <ul class="sidebar-nav" id="sidebar-nav">
-    <li class="nav-item">
-      <a class="nav-link"  href="#">
-        <i class="bi bi-person-fill"></i><span>Profile</span>
-      </a>
-    </li>
-
-    <li class="nav-item">
-      <a class="nav-link" href="#">
-        <i class="bi bi-pencil-square"></i><span>Post</span>
-      </a>
-    </li>
-
-    <li class="nav-item">
-      <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
-        <i class="bi bi-pencil-square"></i><span>Approval</span><i class="bi bi-chevron-down ms-auto"></i>
-      </a>
-      <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-        <li>
-          <a href="<?= base_url('admin/addpost');?>">
-            <i class="bi bi-plus-square"></i><span>List Post</span>
-          </a>
-            <a href="#">
-              <i class="bi bi-clock"></i><span>Waiting for Review</span>
+    <?php if ($this->session->userdata('role_id') == 1): ?>
+        <ul class="sidebar-nav" id="sidebar-nav">
+          <li class="nav-item">
+            <a class="nav-link"  href="#">
+              <i class="bi bi-person-fill"></i><span>Profile</span>
             </a>
-        </li>
-        <li>
-            <a href="#">
-              <i class="bi bi-check-lg"></i><span>Approved</span>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+              <i class="bi bi-pencil-square"></i><span>Approval</span><i class="bi bi-chevron-down ms-auto"></i>
             </a>
-        </li>
-        <li>
-          <a href="<?= base_url('admin/category-post');?>">
-            <i class="bi bi-pencil"></i><span>Category Post</span>
-          <a href="#">
-            <i class="bi bi-x-lg"></i><span>Waiting for Review</span>
-          </a>
-        </li>
-      </ul>
-    </li><!-- End Components Nav -->
+            <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+              <li>
+                  <a href="#">
+                    <i class="bi bi-clock"></i><span>Waiting for Review</span>
+                  </a>
+              </li>
+              <li>
+                  <a href="#">
+                    <i class="bi bi-check-lg"></i><span>Approved</span>
+                  </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="bi bi-x-lg"></i><span>Rejected</span>
+                </a>
+              </li>
+            </ul>
+          </li><!-- End Components Nav -->
 
-    <li class="nav-item">
-      <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
-        <i class="bi bi-person"></i><span>Manajement User</span><i class="bi bi-chevron-down ms-auto"></i>
-      </a>
-      <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-        <li>
-          <a href="forms-elements.html">
-            <i class="bi bi-circle"></i><span>Data User</span>
-          </a>
-        </li>
-      </ul>
-    </li><!-- End Forms Nav -->
-
-    <li class="nav-item">
-      <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
-        <i class="bi bi-gear"></i><span>Manajement Sistem</span><i class="bi bi-chevron-down ms-auto"></i>
-      </a>
-      <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-        <li>
-          <a href="tables-general.html">
-            <i class="bi bi-circle"></i><span>General Tables</span>
-          </a>
-        </li>
-        <li>
-          <a href="tables-data.html">
-            <i class="bi bi-circle"></i><span>Data Tables</span>
-          </a>
-        </li>
-      </ul>
-    </li><!-- End Tables Nav -->
-
-    
-    <!-- End Components Nav -->
-  </ul>
-
+          <li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#components-nav-2" data-bs-toggle="collapse" href="#">
+              <i class="bi bi-pencil-square"></i><span>Management Post</span><i class="bi bi-chevron-down ms-auto"></i>
+            </a>
+            <ul id="components-nav-2" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+              <li>
+                <a href="<?= base_url('admin/addpost');?>">
+                  <i class="bi bi-plus-square"></i><span>List Post</span>
+                </a>
+              </li>
+              <li>
+                <a href="<?= base_url('admin/category-post');?>">
+                  <i class="bi bi-pencil"></i><span>Category Post</span>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <!-- End Components Nav -->
+        </ul>
+    <?php elseif ($this->session->userdata('role_id') == 2): ?>
+      <ul class="sidebar-nav" id="sidebar-nav">
+          <li class="nav-item">
+            <a class="nav-link"  href="#">
+              <i class="bi bi-person-fill"></i><span>Profile</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="bi bi-pencil-square"></i><span>Post</span>
+            </a>
+          </li>
+        </ul>
+    <?php endif; ?>
+  
 </aside>
