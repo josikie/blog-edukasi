adc0b851b87a806193255a7c73b87701c737f3e2
Sutris122 <sutriss122@gmail.com>
2024-07-30 00:38:07 +0700

    update

--- application/views/frontend/includes/header.php
+++ application/views/frontend/includes/header.php
@@ -59,8 +59,9 @@
                                         </ul>
                                     </nav>
                                 </div>
-                            </div>             
-                            <div class="col-xl-2 col-lg-2 col-md-4">
+                            </div>
+                            <a class="btn btn-primary btn-sm " href="login">Login</a>
+                            <!-- <div class="col-xl-2 col-lg-2 col-md-4">
                                 <div class="header-right-btn f-right d-none d-lg-block">
                                     <i class="fas fa-search special-tag"></i>
                                     <div class="search-box">
@@ -70,7 +71,7 @@
                                         </form>
                                     </div>
                                 </div>
-                            </div>
+                            </div> -->
                             <!-- Mobile Menu -->
                             <div class="col-12">
                                 <div class="mobile_menu d-block d-md-none"></div>
